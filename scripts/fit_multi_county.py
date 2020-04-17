6a0c13693ba1bda12fe983546f47735772dbc582
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2020-04-17 01:21:00 -0400

    Merge AD

--- scripts/fit_multi_county.py
+++ scripts/fit_multi_county.py
@@ -33,11 +33,15 @@ import urllib.request
 
 if not os.path.exists('us-counties.csv'):
     urllib.request.urlretrieve(
-        'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',
-        'us-counties.csv')
+        'https://raw.githubusercontent.com/nytimes/'
+        'covid-19-data/master/us-counties.csv',
+        'us-counties.csv'
+    )
     urllib.request.urlretrieve(
-        'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv',
-        'us-states.csv')
+        'https://raw.githubusercontent.com/nytimes/'
+        'covid-19-data/master/us-states.csv',
+        'us-states.csv'
+    )
 
 # Determine the 5 biggest county case rates in these 5 states:
 # NY, NJ, CA, MI, PA, TX
@@ -490,3 +494,53 @@ legend_list = ['Current Mobility', '20% Mobility', '50% Mobility',
 data_list, day_list = fp.get_arrays(fp.get_scenario_dict(fp.scenario_list),
                                     fp.scenario_list, fp.population)
 fp.plot_data(data_list, day_list, legend_list, 0)
+fp.plot_data(data_list, day_list, legend_list, 1)
+
+
+# Anurag's addition (TODO: correct spot for this?)
+# Determine the 5 biggest county case rates in these 5 states:
+def get_county_state_dict(path):
+    top5_df = pd.read_excel(path)
+
+    state_list = top5_df['State'].unique().tolist()
+
+    county_dict = {}
+    for state in state_list:
+        county_dict[state] = top5_df[top5_df['State'] == state][
+            'Counties'].tolist()
+    return pd.DataFrame.from_dict(county_dict)
+
+
+def get_population_dict(path):
+    df = pd.read_excel(path, skiprows=2, skipfooter=5)
+    new_df = df[['Geographic Area', 'Unnamed: 12']].reset_index().iloc[
+             1:].reset_index()
+    Area_list = new_df['Geographic Area']
+    area_list = [i.split(',')[0].split(' ')[0].replace('.', '') for i in
+                 Area_list]
+    new_df['Geographic Area'] = area_list
+    return new_df
+
+
+state_name = 'Texas'
+county_df = get_county_state_dict(path='Top5counties.xlsx')
+
+pop_df = get_population_dict(
+    "https://www2.census.gov/programs-surveys/popest/tables/2010-2019"
+    "/counties/totals/co-est2019-annres-48.xlsx "
+)
+
+
+# NY, NJ, CA, MI, PA, TX
+def get_county_pop_list(county_df, pop_df):
+    county_list = county_df[state_name].tolist()
+    new_pop_df = pop_df[pop_df['Geographic Area'].isin(county_list)]
+    pop_list = new_pop_df['Unnamed: 12'].tolist()
+    return county_list, pop_list
+
+
+county_list, pop_list = get_county_pop_list(county_df, pop_df)
+counties = []
+hospital_beds = [19000, 14000, 5000, 5000, 7893]
+for i in range(len(county_list)):
+    counties.append([county_list[i], state_name, pop_list[i], hospital_beds[i]])
--- scripts/forecast_plotter.py
+++ scripts/forecast_plotter.py
@@ -5,24 +5,30 @@ import seaborn as sns
 import matplotlib.ticker as mtick
 import matplotlib as mpl
 
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from collections import OrderedDict
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', -1)
 
-scenario_list = ['Current Mobility', '20% Return to Normal', '50% Return to Normal', 'Return to Normal Mobility']
+scenario_list = ['Current Mobility', '20% Return to Normal',
+                 '50% Return to Normal', 'Return to Normal Mobility']
+
 
 def get_predictions(path):
     return np.load(path, allow_pickle=True)
 
+
 def get_scenario_dict(scenario_list):
     pathlist = ["Average Case " + i + '.npy' for i in scenario_list]
-    dict ={}
+    # pathlist = ["Average Case " + i + county_name + '.npy' for i in
+    #             scenario_list]
+    dict = {}
     for i in range(len(pathlist)):
         dict[scenario_list[i]] = get_predictions(pathlist[i])
     return dict
+
 
 # Keys
 # Active Cases (observed)
@@ -32,21 +38,26 @@ def get_scenario_dict(scenario_list):
 # Active Cases (latent)
 
 population = 2003554
+
+
 def get_arrays(dict, scenario_list, population):
     data_list = []
     day_list = []
 
     for scenario in scenario_list:
-
         avg_arr = dict[scenario]
 
         # Creating the key based arrays. Have this in casee want to access the specific arrays
-        total_deaths_arr = avg_arr.item().get('Total Deaths')*population
-        active_cases_obs_arr = avg_arr.item().get('Active Cases (observed)')*population
-        hospitalized_arr = avg_arr.item().get('Hospitalized')*population
-        Total_cases_obs = avg_arr.item().get('Total Cases (observed)')*population
-        Total_cases_lat = avg_arr.item().get('Total Cases (latent)')*population
-        Active_cases_lat = avg_arr.item().get('Active Cases (latent)')*population
+        total_deaths_arr = avg_arr.item().get('Total Deaths') * population
+        active_cases_obs_arr = avg_arr.item().get(
+            'Active Cases (observed)') * population
+        hospitalized_arr = avg_arr.item().get('Hospitalized') * population
+        Total_cases_obs = avg_arr.item().get(
+            'Total Cases (observed)') * population
+        Total_cases_lat = avg_arr.item().get(
+            'Total Cases (latent)') * population
+        Active_cases_lat = avg_arr.item().get(
+            'Active Cases (latent)') * population
 
         data_arr = np.array([])
 
@@ -54,14 +65,14 @@ def get_arrays(dict, scenario_list, population):
         # Organize it based on the way you want
 
         data_arr = np.vstack((active_cases_obs_arr, Total_cases_obs))
-        data_arr = np.vstack((data_arr,total_deaths_arr))
-        data_arr = np.vstack((data_arr,hospitalized_arr))
+        data_arr = np.vstack((data_arr, total_deaths_arr))
+        data_arr = np.vstack((data_arr, hospitalized_arr))
 
         # Left out since they were not needed for a certain case but can be uncommented
-        #data_arr = np.vstack((data_arr, Total_cases_lat))
-        #data_arr = np.vstack((data_arr, total_deaths_arr))
+        # data_arr = np.vstack((data_arr, Total_cases_lat))
+        # data_arr = np.vstack((data_arr, total_deaths_arr))
 
-        day_arr = np.arange(0,len(total_deaths_arr),1)
+        day_arr = np.arange(0, len(total_deaths_arr), 1)
 
         data_list.append(data_arr)
         day_list.append(day_arr)
@@ -70,8 +81,6 @@ def get_arrays(dict, scenario_list, population):
 
 
 def plot_data(data_list, day_list, legend_list, gridplot):
-
-
     # Plotting parameters
     plt.figure(dpi=100)
     plt.rcParams['axes.linewidth'] = 1
@@ -91,28 +100,32 @@ def plot_data(data_list, day_list, legend_list, gridplot):
     palette = sns.color_palette("mako_r", 6)
     count = 0
 
-
     # Set of tunable parameters for the plots
     linewidth = 2
-    color_list = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
+    color_list = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink',
+                  'gray', 'olive', 'cyan']
     map_list = ['a', 'b', 'c', 'd']
     xlabel = "Months"
-    ylabel = "Number of cases"
+    ylabel = "Number of cases (in thousands)"
     x_labelpad = 10
     y_labelpad = 30
 
     xtick_rotation = 45
 
-    dates = ["2020-02-21","2020-08-21"]   # the range of dates you want the generated list to be
-    start, end  =[datetime.strptime(_,"%Y-%m-%d") for _ in dates]
-    labels = lambda x,y: list(OrderedDict(((x+ timedelta(_)).strftime(r"%b"), None) for _ in range((y - x).days)).keys())
-    max_days = (end-start).days
-    xticklabel = labels(start,end)
+    dates = ["2020-02-21",
+             "2020-08-21"]  # the range of dates you want the generated list to be
+    start, end = [datetime.strptime(_, "%Y-%m-%d") for _ in dates]
+    labels = lambda x, y: list(OrderedDict(
+        ((x + timedelta(_)).strftime(r"%b"), None) for _ in
+        range((y - x).days)).keys())
+    max_days = (end - start).days
+    xticklabel = labels(start, end)
 
     legend_size = 13
 
     # Change the title, axis, label and tick font settings here
-    title_font = {'fontname': 'Arial', 'size': '16', 'color': 'black', 'weight': 'bold',
+    title_font = {'fontname': 'Arial', 'size': '16', 'color': 'black',
+                  'weight': 'bold',
                   'verticalalignment': 'bottom'}  # Bottom vertical alignment for more space
     axis_font = {'fontname': 'Arial', 'size': '14', 'weight': 'bold'}
     sp_label_font = {'fontname': 'Arial', 'size': '15', 'weight': 'bold'}
@@ -121,25 +134,30 @@ def plot_data(data_list, day_list, legend_list, gridplot):
 
     log_scale = 0
 
-    if (gridplot!=1):
+    if (gridplot != 1):
         for i in range(len(data_list)):
             day_arr = day_list[i]
             data_arr = data_list[i]
-            label_string1 = map_list[i] + "1. {} (Active)".format(legend_list[i])
+            label_string1 = map_list[i] + "1. {} (Active)".format(
+                legend_list[i])
             label_string2 = map_list[i] + "1. {} (Total)".format(legend_list[i])
-            plt.plot(day_arr[0:max_days], data_arr[0][0:max_days], label=label_string1, linestyle="--",
+            plt.plot(day_arr[0:max_days], data_arr[0][0:max_days],
+                     label=label_string1, linestyle="--",
                      color=color_list[i], linewidth=linewidth)
-            plt.plot(day_arr[0:max_days], data_arr[1][0:max_days], label=label_string2,
+            plt.plot(day_arr[0:max_days], data_arr[1][0:max_days],
+                     label=label_string2,
                      color=color_list[i], linewidth=linewidth)
 
         plt.xlabel(xlabel, **sp_label_font, labelpad=x_labelpad)
         plt.ylabel(ylabel, **sp_label_font, labelpad=y_labelpad)
 
-        plt.xticks(range(0, max_days, int(max_days/len(xticklabel))), xticklabel, rotation=xtick_rotation,
+        plt.xticks(range(0, max_days, int(max_days / len(xticklabel))),
+                   xticklabel, rotation=xtick_rotation,
                    **tick_font)
         if (log_scale):
             plt.yscale('log')
-        plt.yticks(np.arange(0, 1000000, 200000), [0, 200, 400, 600, 800], **tick_font)
+        plt.yticks(np.arange(0, 1000000, 200000), [0, 200, 400, 600, 800],
+                   **tick_font)
         plt.legend(prop={'size': legend_size, 'weight': 'bold'})
         plt.margins(0)
         manager = plt.get_current_fig_manager()
@@ -151,49 +169,53 @@ def plot_data(data_list, day_list, legend_list, gridplot):
         elif plot_backend == 'Qt4Agg':
             manager.window.showMaximized()
         # plt.tight_layout(pad=0.5)
-        #plt.savefig("Bexar_total.pdf")
+        # plt.savefig("Bexar_total.pdf")
         plt.show()
 
     else:
-        fig, ax = plt.subplots(1,2)
+        fig, ax = plt.subplots(1, 2)
 
-        while(count_y <2):
+        while (count_y < 2):
             ax1 = ax[count_y]
             ax1.grid(False)
-            print (count_y)
+            print(count_y)
             for i in range(len(data_list)):
                 day_arr = day_list[i]
                 data_arr = data_list[i]
                 if (count_y):
                     temp = 1
-                    label_string = map_list[i] + ". " + legend_list[i] + " (Total)"
+                    label_string = map_list[i] + ". " + legend_list[
+                        i] + " (Total)"
                     linestyle = "-"
                 else:
-                    temp =0
-                    label_string = map_list[i] + ". " + legend_list[i] +" (Active)"
+                    temp = 0
+                    label_string = map_list[i] + ". " + legend_list[
+                        i] + " (Active)"
                     linestyle = "--"
-                ax1.plot(day_arr[0:max_days], data_arr[temp][0:max_days], label=label_string, linestyle=linestyle,
+                ax1.plot(day_arr[0:max_days], data_arr[temp][0:max_days],
+                         label=label_string, linestyle=linestyle,
                          color=color_list[i], linewidth=linewidth)
 
             ax1.set_xlabel(xlabel, **sp_label_font)
             ax1.set_ylabel(ylabel, **sp_label_font)
 
-            ax1.set_xticks(range(0, max_days, int(max_days/len(xticklabel))))
-            ax1.set_xticklabels(xticklabel, rotation=xtick_rotation, **tick_font)
+            ax1.set_xticks(range(0, max_days, int(max_days / len(xticklabel))))
+            ax1.set_xticklabels(xticklabel, rotation=xtick_rotation,
+                                **tick_font)
             if (log_scale):
                 ax1.set_yscale('log')
             ax1.legend(frameon=False)
-            #ax1.set_title(title, ** title_font)
+            # ax1.set_title(title, ** title_font)
             ax1.set_yticklabels(ax1.get_yticks(), **tick_font)
             mf = mtick.ScalarFormatter(useMathText=True)
             mf.set_powerlimits((-5, 5))
             ax1.yaxis.set_major_formatter(mf)
-            count_y +=1
-        #fig.suptitle("Cases based on mobility " , y=0.95, fontweight='bold', **title_font)
-        #fig.text(0.5, 0.03, 'Days (Feb 23 - April 5, 2020)', ha='center', **axis_font)
-        #fig.text(0.05, 0.5, 'Percent change', va='center', rotation='vertical', **axis_font)
-        #fig.tight_layout()
-        #plt.savefig("_split_plots.pdf", orientation='portrait')
+            count_y += 1
+        # fig.suptitle("Cases based on mobility " , y=0.95, fontweight='bold', **title_font)
+        # fig.text(0.5, 0.03, 'Days (Feb 23 - April 5, 2020)', ha='center', **axis_font)
+        # fig.text(0.05, 0.5, 'Percent change', va='center', rotation='vertical', **axis_font)
+        # fig.tight_layout()
+        # plt.savefig("_split_plots.pdf", orientation='portrait')
         plt.show(fig)
 
 # legend_list = ['Current Mobility', '20% Mobility', '50% Mobility', 'Normal Mobility']
