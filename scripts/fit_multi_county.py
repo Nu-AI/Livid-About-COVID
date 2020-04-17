a82623a5ce78e8a524241c08f101175ff397bed0
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2020-04-16 20:43:07 -0400

    Added missing ')'

--- scripts/fit_multi_county.py
+++ scripts/fit_multi_county.py
@@ -16,7 +16,7 @@ from torch import optim
 # root of workspace
 ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
 sys.path.append(ROOT_DIR)
-#import SIRNet
+# import SIRNet
 from SIRNet import sirnet
 
 ## Assumptions: Let's put these properties right up front where they belong ###
@@ -41,7 +41,8 @@ if not os.path.exists('us-counties.csv'):
 # NY, NJ, CA, MI, PA, TX
 counties = []
 counties.append(['New York City', 'New York', 8.0e6, 32000])
-counties.append(['Bexar', 'Texas', 1.99e6, 7793])  # county, state, population, hospital beds
+counties.append(['Bexar', 'Texas', 1.99e6,
+                 7793])  # county, state, population, hospital beds
 
 ## Iterate through counties ##
 ##############################
@@ -68,36 +69,45 @@ for county_data in counties:
     cases = shift_cases
 
     # Load Activity Data
-    state_path = os.path.join(ROOT_DIR, 'data','Mobility data', '{}_mobility.csv'.format(state_name))
+    state_path = os.path.join(ROOT_DIR, 'data', 'Mobility data',
+                              '{}_mobility.csv'.format(state_name))
     if os.path.exists(state_path):
-      mkeys = ['Retail & recreation', 'Grocery & pharmacy', 'Parks', 'Transit stations', 'Workplace', 'Residential']
-      mobilities = {}
-      for category in mkeys:
-          mobilities[category] = OrderedDict()
-          with open(os.path.join(ROOT_DIR, 'data','Mobility data', '{}_mobility.csv'.format(state_name)),'r') as f:
-              csv_reader = csv.reader(f, delimiter=',')
-              header = next(csv_reader)
-              for row in csv_reader:
-                  if row[1] == state_name and row[2].replace(' County', '') == county_name and row[3] == category:
-                      dates = eval(row[6])
-                      vals = eval(row[7])
-                      for i, date in enumerate(dates):
-                          mobilities[category][date] = vals[i]
+        mkeys = ['Retail & recreation', 'Grocery & pharmacy', 'Parks',
+                 'Transit stations', 'Workplace', 'Residential']
+        mobilities = {}
+        for category in mkeys:
+            mobilities[category] = OrderedDict()
+            with open(os.path.join(ROOT_DIR, 'data', 'Mobility data',
+                                   '{}_mobility.csv'.format(state_name)),
+                      'r') as f:
+                csv_reader = csv.reader(f, delimiter=',')
+                header = next(csv_reader)
+                for row in csv_reader:
+                    if row[1] == state_name and row[2].replace(' County',
+                                                               '') == county_name and \
+                            row[3] == category:
+                        dates = eval(row[6])
+                        vals = eval(row[7])
+                        for i, date in enumerate(dates):
+                            mobilities[category][date] = vals[i]
     else:
-      print('No county-level mobility available.  Reverting to state')
-      mkeys = ['Retail & recreation', 'Grocery & pharmacy', 'Parks', 'Transit stations', 'Workplace', 'Residential']
-      mobilities = {}
-      for category in mkeys:
-          mobilities[category] = OrderedDict()
-          with open(os.path.join(ROOT_DIR, 'data','Mobility data', 'US_mobility.csv'),'r') as f:
-              csv_reader = csv.reader(f, delimiter=',')
-              header = next(csv_reader)
-              for row in csv_reader:
-                  if row[1] == 'US' and row[2] == state_name and row[3] == category:
-                      dates = eval(row[6])
-                      vals = eval(row[7])
-                      for i, date in enumerate(dates):
-                          mobilities[category][date] = vals[i]
+        print('No county-level mobility available.  Reverting to state')
+        mkeys = ['Retail & recreation', 'Grocery & pharmacy', 'Parks',
+                 'Transit stations', 'Workplace', 'Residential']
+        mobilities = {}
+        for category in mkeys:
+            mobilities[category] = OrderedDict()
+            with open(os.path.join(ROOT_DIR, 'data', 'Mobility data',
+                                   'US_mobility.csv'), 'r') as f:
+                csv_reader = csv.reader(f, delimiter=',')
+                header = next(csv_reader)
+                for row in csv_reader:
+                    if row[1] == 'US' and row[2] == state_name and row[
+                        3] == category:
+                        dates = eval(row[6])
+                        vals = eval(row[7])
+                        for i, date in enumerate(dates):
+                            mobilities[category][date] = vals[i]
 
     # Rearrange activity data
     mobility = OrderedDict()
@@ -114,10 +124,12 @@ for county_data in counties:
 
     # Estimate hospitalization rate
     p = []
-    df = pd.read_csv(os.path.join(ROOT_DIR, 'data', 'US_County_AgeGrp_2018.csv', encoding="cp1252"))
-#     df = pd.read_csv(
-#         os.path.join('US_County_AgeGrp_2018.csv'),
-#         encoding="cp1252")
+    df = pd.read_csv(
+        os.path.join(ROOT_DIR, 'data', 'US_County_AgeGrp_2018.csv'),
+        encoding="cp1252")
+    #     df = pd.read_csv(
+    #         os.path.join('US_County_AgeGrp_2018.csv'),
+    #         encoding="cp1252")
     print(df.keys())
     a = df.loc[df['STNAME'] == 'Texas'].loc[df['CTYNAME'] == ' County']
     print(a)
@@ -150,7 +162,7 @@ for county_data in counties:
     rates = []
 
     hr_filename = os.path.join(ROOT_DIR, 'data', 'covid_hosp_rate_by_age.csv')
-    #hr_filename = os.path.join('covid_hosp_rate_by_age.csv')
+    # hr_filename = os.path.join('covid_hosp_rate_by_age.csv')
     with open(hr_filename, 'r', encoding='mac_roman') as f:
         csv_reader = csv.reader(f, delimiter=',')
         header = next(csv_reader)
@@ -193,10 +205,12 @@ for county_data in counties:
     ###################### Formatting Data ######################
     #############################################################
 
-    data = np.asarray(data).astype(float)  # Data is 6 columns of mobility, 1 column of case number
+    data = np.asarray(data).astype(
+        float)  # Data is 6 columns of mobility, 1 column of case number
     data = data[5:, :]  # Skip 5 days until we have 10+ patients
 
-    data[:, :6] = (1.0 + data[:, :6] / 100.0)  # convert percentages of change to fractions of activity
+    data[:, :6] = (1.0 + data[:,
+                         :6] / 100.0)  # convert percentages of change to fractions of activity
     print(np.asarray(data).shape)
 
     # Split into input and output data
@@ -221,7 +235,8 @@ for county_data in counties:
 
     def build_model(i0, b_lstm=False, update_k=False):
         model = torch.nn.Sequential()
-        model.add_module("name", sirnet.SEIRNet(i0=i0, b_lstm=b_lstm, update_k=update_k))
+        model.add_module("name", sirnet.SEIRNet(i0=i0, b_lstm=b_lstm,
+                                                update_k=update_k))
         return model
 
 
@@ -232,11 +247,11 @@ for county_data in counties:
 
         hx, fx = model.forward(x)
 
-        #output = loss.forward(fx, y)
-        output = loss.forward(torch.log(fx), torch.log(y) )
+        # output = loss.forward(fx, y)
+        output = loss.forward(torch.log(fx), torch.log(y))
         output.backward()
         optimizer.step()
-        #for name, param in model.named_parameters():
+        # for name, param in model.named_parameters():
         #    if name != "name.h2o.weight":
         #        param.data.clamp_(1e-4)
         return output.data.item()
@@ -297,59 +312,63 @@ for county_data in counties:
     plt.legend(pcs, ['Ground Truth', 'Predicted'])
     plt.show()
 
-
     ######## Forecast mobility from 0 to 100 % ######################################
     #################################################################################
-    for i in range(0,101,10):
-      p = i / 100.0
-      #xN = torch.Tensor(np.ones((1, 6)).astype(np.float32)) * p + X[-1, :, :] * (1-p)
-      xN = torch.Tensor(np.ones((1, 6)).astype(np.float32)) * p
-      print (xN)
-      rX = torch.stack([xN.clone() for i in range(200)])  # 80 x 1 x 6
-      rX = torch.cat((X, rX), axis=0)
-      
-      # Give mobility number as percentage, exclude residences
-      pct = int(np.mean(np.squeeze(xN.numpy())[:5])*100)
+    for i in range(0, 101, 10):
+        p = i / 100.0
+        # xN = torch.Tensor(np.ones((1, 6)).astype(np.float32)) * p + X[-1, :, :] * (1-p)
+        xN = torch.Tensor(np.ones((1, 6)).astype(np.float32)) * p
+        print(xN)
+        rX = torch.stack([xN.clone() for i in range(200)])  # 80 x 1 x 6
+        rX = torch.cat((X, rX), axis=0)
 
-      sir_state, total_cases = model(Variable(rX, requires_grad=False))
-      YY = np.squeeze(np.squeeze(total_cases.detach().numpy()))
+        # Give mobility number as percentage, exclude residences
+        pct = int(np.mean(np.squeeze(xN.numpy())[:5]) * 100)
 
-      # Plot the SIR state
-      s = np.squeeze(sir_state.detach().numpy())
-      days = range(s.shape[0])
-      days = np.array(days)
-      bed40 = beds / population * np.ones((len(days),)) * bed_pct * population
-      bed70 = beds / population * np.ones((len(days),)) * .70 * population
-      active = s[:, 0] * reporting_rate * population
-      total = (s[:, 0] + s[:, 1]) * reporting_rate * population
-      hospitalized = s[:, 0] * float(hosp_rate) * reporting_rate * population
-      total_deaths = s[:, 1] * 0.034 * reporting_rate * population  # recovered * WHO mortality rate (recovered is actually recovered + deceased)
-      print ('HTF? ', s[-1,1], 0.034, reporting_rate, population)
+        sir_state, total_cases = model(Variable(rX, requires_grad=False))
+        YY = np.squeeze(np.squeeze(total_cases.detach().numpy()))
 
-      fig, axs = plt.subplots(2)
-      axs[0].plot(days, active, 'b', days, total, 'r')
-      axs[0].legend(['Active', 'Total'], loc='upper left')
-      axs[0].set(ylabel='Cases')
-      axs[0].set_title('Quarantine/Normal: {:02d}/{:02d} | Avg. Mobility: {:02d}%'.format(100-i,i,pct))
-      axs[0].set_xticklabels([])
+        # Plot the SIR state
+        s = np.squeeze(sir_state.detach().numpy())
+        days = range(s.shape[0])
+        days = np.array(days)
+        bed40 = beds / population * np.ones((len(days),)) * bed_pct * population
+        bed70 = beds / population * np.ones((len(days),)) * .70 * population
+        active = s[:, 0] * reporting_rate * population
+        total = (s[:, 0] + s[:, 1]) * reporting_rate * population
+        hospitalized = s[:, 0] * float(hosp_rate) * reporting_rate * population
+        total_deaths = s[:,
+                       1] * 0.034 * reporting_rate * population  # recovered * WHO mortality rate (recovered is actually recovered + deceased)
+        print('HTF? ', s[-1, 1], 0.034, reporting_rate, population)
 
-      axs[1].plot(days,hospitalized,'b', days,total_deaths, 'k',days, bed40, 'c', bed70,'r')
-      axs[1].legend(['Hospitalized','Total Deaths','40% of Hospital Beds', '70% of Hospital Beds'], loc='upper left')
-      axs[1].set(xlabel='Months',ylabel='Cases')
-      tick_font = {'fontname': 'Arial', 'size': '11', 'weight': 'bold'}
-      axs[1].set_xticks(range(0,211,30))
-      axs[1].set_xticklabels(['March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct'])
+        fig, axs = plt.subplots(2)
+        axs[0].plot(days, active, 'b', days, total, 'r')
+        axs[0].legend(['Active', 'Total'], loc='upper left')
+        axs[0].set(ylabel='Cases')
+        axs[0].set_title(
+            'Quarantine/Normal: {:02d}/{:02d} | Avg. Mobility: {:02d}%'.format(
+                100 - i, i, pct))
+        axs[0].set_xticklabels([])
 
-      #plt.savefig('output/mobility{:03d}.png'.format(i))
-      plt.show()
-      plt.close()
+        axs[1].plot(days, hospitalized, 'b', days, total_deaths, 'k', days,
+                    bed40, 'c', bed70, 'r')
+        axs[1].legend(['Hospitalized', 'Total Deaths', '40% of Hospital Beds',
+                       '70% of Hospital Beds'], loc='upper left')
+        axs[1].set(xlabel='Months', ylabel='Cases')
+        tick_font = {'fontname': 'Arial', 'size': '11', 'weight': 'bold'}
+        axs[1].set_xticks(range(0, 211, 30))
+        axs[1].set_xticklabels(
+            ['March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct'])
 
-
+        # plt.savefig('output/mobility{:03d}.png'.format(i))
+        plt.show()
+        plt.close()
 
     ######## Forecast 200 more days at current quarantine mobility ##################
     #################################################################################
-    xN = X[-1, :, :]   # lockdown
-    xN[0,:] = torch.Tensor(np.array([.1, .1, .1, .1, .1, 3]).astype(np.float32))
+    xN = X[-1, :, :]  # lockdown
+    xN[0, :] = torch.Tensor(
+        np.array([.1, .1, .1, .1, .1, 3]).astype(np.float32))
     qX = torch.stack([xN.clone() for i in range(200)])  # 200 x 1 x 6
     qX = torch.cat((X, qX), axis=0)
 
@@ -403,7 +422,8 @@ for county_data in counties:
 
     ######## Forecast 120 more days at 25%-return to normal mobility ###############
     #################################################################################
-    xN = torch.Tensor(np.ones((1, 6)).astype(np.float32)) * .20 + X[-1, :, :] * .80
+    xN = torch.Tensor(np.ones((1, 6)).astype(np.float32)) * .20 + X[-1, :,
+                                                                  :] * .80
     rX = torch.stack([xN.clone() for i in range(200)])  # 80 x 1 x 6
     rX = torch.cat((X, rX), axis=0)
 
@@ -423,7 +443,7 @@ for county_data in counties:
     hosp_rate = float(hosp_rate)
     days = range(sir_state1.shape[0])
     bedp = beds / population * np.ones((len(days),)) * bed_pct
-    print (sir_state1.shape, hosp_rate, reporting_rate)
+    print(sir_state1.shape, hosp_rate, reporting_rate)
     hosp_qrt = sir_state1[:, 0] * hosp_rate * reporting_rate
     hosp_nom = sir_state2[:, 0] * hosp_rate * reporting_rate
     hosp_hlf = sir_state3[:, 0] * hosp_rate * reporting_rate
@@ -436,12 +456,14 @@ for county_data in counties:
                    days, bedp, 'b')
     plt.xlabel('Day')
     plt.ylabel('Hospitalized')
-    plt.legend(pcs, ['Current Mobility', 'Return to Normal Mobility', '50% Return to Normal', '20% Return to Normal',
+    plt.legend(pcs, ['Current Mobility', 'Return to Normal Mobility',
+                     '50% Return to Normal', '20% Return to Normal',
                      'Available Beds'])
     plt.show()
 
     # Save the data
-    cases = ['Current Mobility', 'Return to Normal Mobility', '50% Return to Normal', '20% Return to Normal']
+    cases = ['Current Mobility', 'Return to Normal Mobility',
+             '50% Return to Normal', '20% Return to Normal']
     for i, s in enumerate([sir_state1, sir_state2, sir_state3, sir_state4]):
         data = {}
         data['Days'] = np.array(days)
@@ -450,17 +472,17 @@ for county_data in counties:
         data['Total Cases (latent)'] = s[:, 0] + s[:, 1]  # infected + recovered
         data['Total Cases (observed)'] = (s[:, 0] + s[:, 1]) * reporting_rate
         data['Hospitalized'] = s[:, 0] * hosp_rate * reporting_rate
-        data['Total Deaths'] = s[:, 1] * 0.034 * reporting_rate  # recovered * WHO mortality rate (recovered is actually recovered + deceased)
+        data['Total Deaths'] = s[:,
+                               1] * 0.034 * reporting_rate  # recovered * WHO mortality rate (recovered is actually recovered + deceased)
         # Alternative way to save
-        #str = 'Average Case ' + str(cases[i]) + str(county_name) + '.npy'
-        #np.save(str, data)
+        # str = 'Average Case ' + str(cases[i]) + str(county_name) + '.npy'
+        # np.save(str, data)
         np.save('Average Case {}.npy'.format(cases[i]), data)
-
-
 
 from . import forecast_plotter as fp
 
-legend_list = ['Current Mobility', '20% Mobility', '50% Mobility', 'Normal Mobility']
-data_list, day_list = fp.get_arrays(fp.get_scenario_dict(fp.scenario_list), fp.scenario_list, fp.population)
-fp.plot_data(data_list, day_list,legend_list, 0)
-
+legend_list = ['Current Mobility', '20% Mobility', '50% Mobility',
+               'Normal Mobility']
+data_list, day_list = fp.get_arrays(fp.get_scenario_dict(fp.scenario_list),
+                                    fp.scenario_list, fp.population)
+fp.plot_data(data_list, day_list, legend_list, 0)
