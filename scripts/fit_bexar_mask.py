88f5b10cabb615ce44189655f62f43629471d5b1
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2020-06-29 00:15:14 -0400

    Minor formatting changes

--- scripts/fit_bexar_mask.py
+++ scripts/fit_bexar_mask.py
@@ -1,13 +1,9 @@
 import os
 import sys
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import torch
 import datetime as dt
-import retrieve_data 
-
-
 
 ############### Paths ##############################
 ####################################################
@@ -24,112 +20,118 @@ if not os.path.exists(RESULTS_DIR):
     os.mkdir(RESULTS_DIR)
 
 from SIRNet import util, trainer
+from scripts import retrieve_data
 
 ########### ASSUMPTIONS ##############################
 ######################################################
+# @formatter:off
 reporting_rate = 0.1     # Portion of cases that are actually detected
 delay_days = 10          # Days between becoming infected / positive confirmation (due to incubation period / testing latency
 start_model = 23         # The day where we begin our fit
-mask_modifier = False    # 
+mask_modifier = False    #
 mask_day = 65            # Day of mask order
+# @formatter:on
 
 ## Cases ##
 actives = {}
 totals = {}
 for reporting_rate in [0.05, 0.1, 0.3]:
 
-  ############## Simplified Data ####################
-  ###################################################
-  paramdict = {}
-  paramdict['country'] = 'United States'
-  paramdict['states'] = ['Texas']
-  paramdict['counties'] = ['Bexar County']
-  df = retrieve_data.get_data(paramdict)
+    ############## Simplified Data ####################
+    ###################################################
+    paramdict = {}
+    paramdict['country'] = 'United States'
+    paramdict['states'] = ['Texas']
+    paramdict['counties'] = ['Bexar County']
+    df = retrieve_data.get_data(paramdict)
 
-  mobility = df[['Retail & recreation', 'Grocery & pharmacy', 'Parks', 'Transit stations', 'Workplace', 'Residential']]
-  cases = df['Cases']
-  day0 = df['date'][0]
-  population = df['Population'][0]
+    mobility = df[['Retail & recreation', 'Grocery & pharmacy', 'Parks',
+                   'Transit stations', 'Workplace', 'Residential']]
+    cases = df['Cases']
+    day0 = df['date'][0]
+    population = df['Population'][0]
 
-  # offset case data by delay days (treat it as though it was recorded earlier)
-  cases = np.array(cases[delay_days:])
-  mobility = np.array(mobility[:-delay_days])
-  county_name = 'Bexar'
+    # offset case data by delay days (treat it as though it was recorded earlier)
+    cases = np.array(cases[delay_days:])
+    mobility = np.array(mobility[:-delay_days])
+    county_name = 'Bexar'
 
-  ###################### Formatting Data ######################
-  #############################################################
-  #print(mobility)
-  mobility[-1][-1] = 17.0
-  mobility = np.asarray(mobility).astype(np.float32)
-  mobility[:, :6] = (1.0 + mobility[:, :6] / 100.0)  # convert percentages of change to fractions of activity
+    ###################### Formatting Data ######################
+    #############################################################
+    mobility[-1][-1] = 17.0
+    mobility = np.asarray(mobility).astype(np.float32)
+    # convert percentages of change to fractions of activity
+    mobility[:, :6] = 1.0 + mobility[:, :6] / 100.0
 
-  # Turn the last column into 1-hot social-distancing enforcement
-  mobility[:,5] = 0
-  if mask_modifier: mobility[mask_day:,5] = 1.0
+    # Turn the last column into 1-hot social-distancing enforcement
+    mobility[:, 5] = 0
+    if mask_modifier:
+        mobility[mask_day:, 5] = 1.0
 
-  # Initial conditions
-  i0 = float(cases[start_model-1]) / population / reporting_rate
-  e0 = 2.2*i0/5.0
-  mobility = mobility[start_model:]  # start with delay
-  cases = cases[start_model:]        # delay days
+    # Initial conditions
+    i0 = float(cases[start_model - 1]) / population / reporting_rate
+    e0 = 2.2 * i0 / 5.0
+    mobility = mobility[start_model:]  # start with delay
+    cases = cases[start_model:]  # delay days
 
-  # Split into input and output data
-  X, Y = mobility, cases
+    # Split into input and output data
+    X, Y = mobility, cases
 
-  # divide out population of county, reporting rate
-  Y = (Y / population) / reporting_rate
+    # divide out population of county, reporting rate
+    Y = (Y / population) / reporting_rate
 
-  # To Torch on device
-  X = torch.from_numpy(X.astype(np.float32))
-  Y = torch.from_numpy(Y.astype(np.float32))
+    # To Torch on device
+    X = torch.from_numpy(X.astype(np.float32))
+    Y = torch.from_numpy(Y.astype(np.float32))
 
-  # Add batch dimension
-  X = X.reshape(X.shape[0], 1, X.shape[1])  # time x batch x channels
-  Y = Y.reshape(Y.shape[0], 1, 1)  # time x batch x channels
+    # Add batch dimension
+    X = X.reshape(X.shape[0], 1, X.shape[1])  # time x batch x channels
+    Y = Y.reshape(Y.shape[0], 1, 1)  # time x batch x channels
 
-  #################### Training #######################
-  #####################################################
-  weights_name = WEIGHTS_DIR + '/{}_weights.pt'.format(county_name)
-  trnr = trainer.Trainer(weights_name)
-  model = trnr.build_model(e0,i0)
-  trnr.train(model, X, Y, 200)
+    #################### Training #######################
+    #####################################################
+    weights_name = WEIGHTS_DIR + '/{}_weights.pt'.format(county_name)
+    trnr = trainer.Trainer(weights_name)
+    model = trnr.build_model(e0, i0)
+    trnr.train(model, X, Y, 200)
 
-  ################ Forecasting #######################
-  ####################################################
-  active = {}
-  total = {}
-  cases = [25, 50, 75, 100]
-  for case in cases:
-      xN = torch.ones((1, 6), dtype=torch.float32) * case/100
-      xN[0,5] = 0
-      rX = xN.expand(200, *xN.shape)  # 200 x 1 x 6
-      rX = torch.cat((X, rX), dim=0)
-      if mask_modifier:
-        rX[mask_day:,0,5] = 1.0
-      sir_state, total_cases = model(rX)
-      s = util.to_numpy(sir_state)
-      active[case] = s[:,0] * reporting_rate * population
-      total[case] = (s[:,0] + s[:,1]) * reporting_rate * population
-  actives[reporting_rate] = active
-  totals[reporting_rate] = total
+    ################ Forecasting #######################
+    ####################################################
+    active = {}
+    total = {}
+    cases = [25, 50, 75, 100]
+    for case in cases:
+        xN = torch.ones((1, 6), dtype=torch.float32) * case / 100
+        xN[0, 5] = 0
+        rX = xN.expand(200, *xN.shape)  # 200 x 1 x 6
+        rX = torch.cat((X, rX), dim=0)
+        if mask_modifier:
+            rX[mask_day:, 0, 5] = 1.0
+        sir_state, total_cases = model(rX)
+        s = util.to_numpy(sir_state)
+        active[case] = s[:, 0] * reporting_rate * population
+        total[case] = (s[:, 0] + s[:, 1]) * reporting_rate * population
+    actives[reporting_rate] = active
+    totals[reporting_rate] = total
 
-  ############## Forecast Dates ####################
-  ##################################################
-  yy, mm, dd = day0.split('-')
-  date0 = dt.datetime(int(yy),int(mm),int(dd))
-  days = np.arange(rX.shape[0])
-  dates = [date0 + dt.timedelta(days=int(d + delay_days + start_model)) for d in days]
+    ############## Forecast Dates ####################
+    ##################################################
+    yy, mm, dd = day0.split('-')
+    date0 = dt.datetime(int(yy), int(mm), int(dd))
+    days = np.arange(rX.shape[0])
+    dates = [date0 + dt.timedelta(days=int(d + delay_days + start_model)) for d
+             in days]
 
-  ############### Reporting #########################
-  ###################################################
-  print('\n#########################################\n\n')
-  timestamp = dt.datetime.now().strftime('%Y_%m_%d')
-  for case in cases:
-      M = np.max(active[case])
-      idx = np.argmax(active[case])
-      print ('Case: {}%'.format(case))
-      print ('  Max value: {}'.format(M))
-      print ('  Day: {}, {}'.format(idx, dates[idx]))
+    ############### Reporting #########################
+    ###################################################
+    print('\n#########################################\n\n')
+    timestamp = dt.datetime.now().strftime('%Y_%m_%d')
+    for case in cases:
+        M = np.max(active[case])
+        idx = np.argmax(active[case])
+        print('Case: {}%'.format(case))
+        print('  Max value: {}'.format(M))
+        print('  Day: {}, {}'.format(idx, dates[idx]))
 
 ############### Plotting ##########################
 ###################################################
@@ -144,13 +146,17 @@ plt.rcParams.update({'font.size': 22})
 
 # Plot 1. Total Cases (Log)
 pidx = gt.shape[0] + 60  # write letter prediction at 60 days in the future
-plt.figure(dpi=100, figsize=(16,8))
+plt.figure(dpi=100, figsize=(16, 8))
 for case in total.keys():
-  plt.plot(dates, totals[.1][case], cs[case], linewidth=4.0, label='{}. {}% Mobility'.format(cl[case], case))
-  #plt.fill_between(dates, totals[.05][case], totals[.30][case], color=cs[case][0], alpha=.1)
-  plt.fill_between(dates, totals[.05][case], totals[.1][case], color=cs[case][0], alpha=.1)
-  plt.fill_between(dates, totals[.1][case], totals[.30][case], color=cs[case][0], alpha=.1)
-  plt.text(dates[pidx],totals[.1][case][pidx],cl[case])
+    plt.plot(dates, totals[.1][case], cs[case], linewidth=4.0,
+             label='{}. {}% Mobility'.format(cl[case], case))
+    # plt.fill_between(dates, totals[.05][case], totals[.30][case],
+    #                  color=cs[case][0], alpha=.1)
+    plt.fill_between(dates, totals[.05][case], totals[.1][case],
+                     color=cs[case][0], alpha=.1)
+    plt.fill_between(dates, totals[.1][case], totals[.30][case],
+                     color=cs[case][0], alpha=.1)
+    plt.text(dates[pidx], totals[.1][case][pidx], cl[case])
 plt.plot(dates[:Y.shape[0]], gt, 'ks', label='SAMHD Data')
 
 plt.title('Total Case Count')
@@ -162,22 +168,29 @@ plt.show()
 
 # Plots 2 & 3. Active Cases (zoomed out and zoomed in)
 for zoom in [True, False]:
-  plt.figure(dpi=100, figsize=(16,8))
-  for case in total.keys():
-    plt.plot(dates, actives[.1][case], cs[case],linewidth=4.0, label='{}. {}% Mobility'.format(cl[case], case))
-    #plt.fill_between(dates, actives[.05][case], actives[.30][case], color=cs[case][0], alpha=.1)
-    plt.fill_between(dates, actives[.05][case], actives[.1][case], color=cs[case][0], alpha=.1)
-    plt.fill_between(dates, actives[.1][case], actives[.30][case], color=cs[case][0], alpha=.1)
-    pidx = gt.shape[0]+10 if zoom else np.argmax(actives[.1][case])  # write at 10 days or peak
-    if case == 50: pidx += 5
-    if zoom:
-      plt.text(dates[pidx],min(actives[.1][case][pidx],1400),cl[case])
-    else:
-      plt.text(dates[pidx],actives[.1][case][pidx],cl[case])
+    plt.figure(dpi=100, figsize=(16, 8))
+    for case in total.keys():
+        plt.plot(dates, actives[.1][case], cs[case], linewidth=4.0,
+                 label='{}. {}% Mobility'.format(cl[case], case))
+        # plt.fill_between(dates, actives[.05][case], actives[.30][case],
+        #                  color=cs[case][0], alpha=.1)
+        plt.fill_between(dates, actives[.05][case], actives[.1][case],
+                         color=cs[case][0], alpha=.1)
+        plt.fill_between(dates, actives[.1][case], actives[.30][case],
+                         color=cs[case][0], alpha=.1)
+        pidx = (gt.shape[0] + 10 if zoom else
+                np.argmax(actives[.1][case]))  # write at 10 days or peak
+        if case == 50:
+            pidx += 5
+        if zoom:
+            plt.text(dates[pidx], min(actives[.1][case][pidx], 1400), cl[case])
+        else:
+            plt.text(dates[pidx], actives[.1][case][pidx], cl[case])
 
-  plt.title('Active (Infectious) Case Count')
-  plt.ylabel('Count')
-  if zoom: plt.ylim((0, gt[-1]))
-  util.plt_setup()
-  plt.savefig(RESULTS_DIR + '/{}_Active_Cases{}.pdf'.format(timestamp, zoom))
-  plt.show()
+    plt.title('Active (Infectious) Case Count')
+    plt.ylabel('Count')
+    if zoom:
+        plt.ylim((0, gt[-1]))
+    util.plt_setup()
+    plt.savefig(RESULTS_DIR + '/{}_Active_Cases{}.pdf'.format(timestamp, zoom))
+    plt.show()
